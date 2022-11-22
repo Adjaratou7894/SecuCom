fa6df211d0029254abd08c2b5e3f6583001fd750
Adjaratou7894 <81575162+Adjaratou7894@users.noreply.github.com>
2022-11-22 17:14:19 +0000

    Creation du projet et ajout des dependances

---
+++ src/main/java/com/securite/secucom/SecuComApplication.java
@@ -0,0 +1,16 @@
+package com.securite.secucom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuComApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuComApplication.class, args);
+        System.out.println("Bienvenue  sur cher admin");
+        System.out.println("Bienvenue  sur cher user");
+
+    }
+
+}
